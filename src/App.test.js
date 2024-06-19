74e827b48036e977a1e6f057ce69c1f2a4853542
srnnnn <tpfla311@naver.com>
2024-06-19 14:42:40 +0900

    0619 act err commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,3 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 import { render, screen } from "@testing-library/react";
 import { act } from "react"; // 여기에 `act`를 `react`에서 가져옵니다.
 import App from "./App";
